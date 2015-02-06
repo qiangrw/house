edd3aaca02407c38045eec767fcd959198c283f0
Runwei Qiang <qiangrw@gmail.com>
2015-02-06 15:26:53 +0800

    whole rent.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function(){
         // TODO signout interface
         location.href='signin.html';
     });
+    $("#update-btn").click(function() {
+        // TODO signout interface
+        
+    });
 });
